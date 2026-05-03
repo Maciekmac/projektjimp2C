28abe223751f2fc3a13ef602a1e683e6288a333f
MikolajDulny <01206761@pw.edu.pl>
2026-05-03 19:45:51 +0200

    „Dodanie klas Vertex i Edge do struktury MVC.

---
+++ czesc2-java/src/model/Edge.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Edge {
+    private Vertex v1;
+    private Vertex v2;
+    private double weight;
+
+    public Edge(Vertex v1, Vertex v2, double weight) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.weight = weight;
+    }
+
+    public Vertex getV1() { return v1; }
+    public Vertex getV2() { return v2; }
+    public double getWeight() { return weight; }
+}
